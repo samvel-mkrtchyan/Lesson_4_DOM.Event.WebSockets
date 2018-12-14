c39e09fd60374ec4ee170c330a04e9d9afa277b1
samvel-mkrtchyan <45490429+samvel-mkrtchyan@users.noreply.github.com>
2018-12-14 11:51:44 +0400

    DOM

---
+++ script.js
@@ -0,0 +1,15 @@
+var clickCount = 0;
+function clickHandler(evt){
+   clickCount++;
+   console.log(evt);
+   var str = "Thanks for clicking " + clickCount;
+   this.innerText = str;
+}
+
+function bodyClick(evt){
+   console.log("Clicked at X: " + evt.pageX + ", Y: " + evt.pageY);
+}
+window.onclick = bodyClick;
+
+var p = document.getElementById("pElement");
+p.addEventListener("click", clickHandler);
